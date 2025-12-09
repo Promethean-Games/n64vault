e8cf3ba311a950561e7e42abd2252b86789d6126
Promethean-Games <alan.acevedo@promethean-games.com>
2025-12-09 10:41:02 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpazEeNdcXp4L0KDg4JQ_cjwT6lgLZWAY",
+  authDomain: "watch-party-v1.firebaseapp.com",
+  databaseURL: "https://watch-party-v1-default-rtdb.firebaseio.com",
+  projectId: "watch-party-v1",
+  storageBucket: "watch-party-v1.firebasestorage.app",
+  messagingSenderId: "686201042880",
+  appId: "1:686201042880:web:55be33ec9df0dd2087ba25",
+  measurementId: "G-BZGPSLHHMZ"
+};
